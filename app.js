9f6f84c67e947ba68d8d2732895732bd0cc17590
bonbonpapa <45809121+bonbonpapa@users.noreply.github.com>
2020-05-12 12:32:08 -0400

    server part for the authentification

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+require("dotenv/config");
 const db_config = require("./config.js");
 const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const { verify } = require("jsonwebtoken");
+const { hash, compare } = require("bcryptjs");
 const mysql = require("mysql");
+const {
+  createAccessToken,
+  createRefreshToken,
+  sendAccessToken,
+  sendRefreshToken,
+} = require("./tokens.js");
+const { fakeDB } = require("./fakeDB.js");
+const { isAuth } = require("./isAuth.js");
 
 //Create connection
 
@@ -21,6 +34,20 @@ db.connect((err) => {
 
 const app = express();
 
+// user express middleware to cookie handling
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+// needed to be able to read body data
+app.use(express.json()); // to support JSON-encoded bodies
+app.use(express.urlencoded({ extended: true })); // support URL-encoded bodies
+
 //create DB
 app.get("/createdb", (req, res) => {
   let sql = "CREATE DATABASE nodemysql";
@@ -40,6 +67,11 @@ app.get("/createpoststable", (req, res) => {
     console.log(result);
     res.send("Post table created");
   });
+});
+
+app.get("/createuserstable", (req, res) => {
+  let sql =
+    "Create Table Users (User_Id Numeric(6) PRIMARY KEY, First_Name Varchar(20),  Last_Name Varchar(25) NOT NULL,  Email Varchar(20))";
 });
 
 // insert post 1
@@ -107,6 +139,124 @@ app.get("/deletepost/:id", (req, res) => {
   });
 });
 
-app.listen("3000", () => {
-  console.log("server started on port 3000");
+// 1. regiser a user
+app.post("/register", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    //1. check if user existed
+
+    const user = fakeDB.find((user) => user.email === email);
+    if (user) throw new Error("User already existed");
+
+    // 2. if use not existed, has the password
+
+    const hashedPassword = await hash(password, 10);
+
+    //3. insert the user in 'database'
+
+    fakeDB.push({
+      id: fakeDB.length,
+      email: email,
+      password: hashedPassword,
+    });
+    res.send({ message: "user created" });
+    console.log(fakeDB);
+  } catch (err) {
+    res.send({
+      error: `${err.message}`,
+    });
+  }
+});
+
+// 2. login a user
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // 1. find user in the database, if not exist send error
+
+    const user = fakeDB.find((user) => user.email === email);
+    if (!user) throw new Error("user not exist");
+    // 2. compare crypted passowrd and see if it checks out
+    const valid = await compare(password, user.password);
+    if (!valid) throw new Error("Passord not corred");
+
+    // 3. create refresh and accesstoken
+    const accesstoken = createAccessToken(user.id);
+    const refreshtoken = createRefreshToken(user.id);
+
+    //4. put refresh token in the database
+    user.refreshtoken = refreshtoken;
+
+    console.log(fakeDB);
+    // 5. send token refreshtoken as a cookie and accesstoken as a regular response
+    sendRefreshToken(res, refreshtoken);
+    sendAccessToken(req, res, accesstoken);
+  } catch (err) {
+    res.send({
+      error: `${err.message}`,
+    });
+  }
+});
+// 3. logout a user
+app.post("/logout", (_req, res) => {
+  res.clearCookie("refreshtoken", { path: "/refresh_token" });
+  return res.send({
+    message: "logged out",
+  });
+});
+
+// 4. setup a protected route
+app.post("/protected", async (req, res) => {
+  try {
+    const userId = isAuth(req);
+    if (userId !== null)
+      res.send({
+        data: "this is protected data",
+      });
+  } catch (err) {
+    res.send({
+      error: `${err.message}`,
+    });
+  }
+});
+
+// 5. get a new accesstoken with a refresh  token
+app.post("/refresh_token", (req, res) => {
+  const token = req.cookies.refreshtoken;
+  // if we don't have a token in our request
+  if (!token)
+    return res.send({
+      accesstoken: "",
+    });
+  // we have a token, let's verify it
+  let payload = null;
+  try {
+    payload = verify(token, process.env.REFRESH_TOKEN_SECRET);
+  } catch (err) {
+    return res.send({ accesstoken: "" });
+  }
+
+  // token is valid, check if user exited
+  const user = fakeDB.find((user) => user.id === payload.userId);
+  if (!user) return res.send({ accesstoken: "" });
+
+  // user exist, check if refreshtoken exist on user
+  if (user.refreshtoken !== token) {
+    return res.send({ accesstoken: "" });
+  }
+  // token exist, create new refresh and accesstoekn
+  const accesstoken = createAccessToken(user.id);
+  const refreshtoken = createRefreshToken(user.id);
+
+  user.refreshtoken = refreshtoken;
+
+  // all good to go, send new refreshtoken and accesstoken
+  sendRefreshToken(res, refreshtoken);
+  return res.send({ accesstoken });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`server listening on PORT ${process.env.PORT}`);
 });
---
+++ fakeDB.js
@@ -0,0 +1,7 @@
+exports.fakeDB = [
+  {
+    id: 0,
+    email: "dui.zhang@gmail.com",
+    password: "$2a$10$B5tvMHpgQMV2x.KfOgyN2On3eEFnFJ73sZAvPiU1Xg2jXZeJFR0lO",
+  },
+];
